ac58b53c448c0d6a858cce59bff9400cb675a46d
Puma-1027 <honda.devpartner1027@gmail.com>
2022-01-28 09:35:25 +0900

    Shopify update
    add the liquid files to theme.

---
+++ assets/skb-1.js
@@ -0,0 +1,9 @@
+// QA
+$(function() {
+  $('.cart_bottom_top').on('click', function () {
+    /*クリックでコンテンツを開閉*/
+    $(this).next().slideToggle(200)
+    /*矢印の向きを変更*/
+    $(this).toggleClass('open', 200)
+  })
+});
